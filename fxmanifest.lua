c758f1aba6ca6dc939644a23f447a172380dded7
YK <yykoo933@gmail.com>
2024-09-06 22:07:52 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'YK'
+description 'https://dsc.gg/ykscripts'
+version '1.0.0'
+
+client_scripts {
+	'client.lua',
+}
+
+server_scripts {
+	'server.lua',
+}
+
+shared_scripts {
+	'@ox_lib/init.lua',
+	'config.lua'
+}
+
