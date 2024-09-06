189c9ceb3f2542f97ea56ddd99e7b45c450a8dff
YK <yykoo933@gmail.com>
2024-09-06 22:08:18 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,12 @@
+YK = {}
+
+YK.Config = {
+    CooldownTime = 300000,  -- Cooldown in ms
+    DrugNeedleChance = 0.1, -- The change of drugneedle 0.1 = 10%
+    DumpsterLoot = { -- Items to find
+        "panties",
+        "cola",
+        "bread",
+        "paperbag"
+    }
+}
